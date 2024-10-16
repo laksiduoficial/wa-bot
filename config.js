5af3258cfd248db4149adfd760529d9f67020acd
laksiduoficial <maheshnimsralaksidu123@gmail.com>
2024-10-16 23:50:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID ,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/f3b0b4e681583334b7330.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "helooooo jsjs",
 };
