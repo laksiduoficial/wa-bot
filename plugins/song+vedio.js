909167c0a2fe3f54e2788484ef266548a04c1bf4
laksiduoficial <maheshnimsralaksidu123@gmail.com>
2024-10-17 09:19:14 +0530

    Update song+vedio.js

--- plugins/song+vedio.js
+++ plugins/song+vedio.js
@@ -1,1 +1,25 @@
+const {cmd , commands} = require('../command')
+const fg = require("api-dylux")
+const yts = require("yt-search")
 
+
+cmd({
+    pattern: "song",
+    desc: "download songs",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("please give me url or title")
+const search = await yts(q)
+const data = search.video[0];
+const url = data.url
+
+let desc = '
+
+}catch{e}{
+console.log(e)
+reply('${e}')
+)
+})
